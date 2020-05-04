3597316489e5889414295107a235858b7c761a81
Laercio Nogueira <laerciopesa30@hotmail.com>
2020-05-04 07:47:52 -0300

    passo 4

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const VueLoaderPlugin = require('vue-loader');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: `${__dirname}/static/`,
+    filename: 'build.js'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node-modules/
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+}
