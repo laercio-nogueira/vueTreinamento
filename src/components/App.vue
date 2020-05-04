2badbdcb2220ae61e5ff351a59399a16a6563cc2
Laercio Nogueira <laerciopesa30@hotmail.com>
2020-05-04 07:58:07 -0300

    Passo 11

---
+++ src/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Ola</div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body {
+  margin: 0 auto;
+  padding: 0 auto;
+}
+div {
+  background: red
+}
+</style>
